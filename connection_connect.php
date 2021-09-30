a229e871957eac52fe4ba75df9d02b2ee476b57a
tanutcha-coder <tanutcha.b@ku.th>
2021-09-30 20:52:54 +0700

    connect

---
+++ connection_connect.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db21_003";
+
+
+// create connection
+$conn = new mysqli($servername, $username, $password);
+mysqli_set_charset($conn, "utf8");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (!$conn->select_db($dbname)) {
+    die("Connection failed : " . $conn->connect_error);
+} else {
+    // echo "connection success";
+}
