bf9b6fbaed0e722f202d287de9df2994560f1469
tanutcha-coder <tanutcha.b@ku.th>
2021-10-03 16:26:29 +0700

    bootstrap

--- index.php
+++ index.php
@@ -10,13 +10,28 @@
     }
 ?>
 <html>
-    <head></head>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="?controller=pages&action=home" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="?controller=quataion&action=index" class="nav-link px-2 text-white">Quatation</a></li>
+          <li><a href="?controller=quataion&action=index" class="nav-link px-2 text-white">QuatationDetail</a></li>
+          <li><a href="?controller=product&action=index" class="nav-link px-2 text-white">productModel</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        
+
+        
+      </div>
+    </div>
+  </header>
         <body>
-            <?php echo "controller =".$controller.",action = ".$action;?>
-            <br>[<a href="?controller=pages&action=home">HOME</a>]
-            [<a href="?controller=quataion&action=index">Quatation</a>]<br>
-            [<a href="?controller=quataion&action=index">QuatationDetail</a>]<br>
-            [<a href="?controller=product&action=index">productModel</a>]<br>
             <?php require_once("routes.php"); ?>
         </body>
     
